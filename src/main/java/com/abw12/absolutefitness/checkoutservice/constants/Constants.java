d5fc92b31934a9b8a285e83b1170b4b33cf84a95
Abhishek Wasave <Abhi_wasave@abw12>
2023-12-23 12:18:40 +0530

    cart validtaion api implemented

--- src/main/java/com/abw12/absolutefitness/checkoutservice/constants/Constants.java
+++ src/main/java/com/abw12/absolutefitness/checkoutservice/constants/Constants.java
@@ -1,8 +1,6 @@
 package com.abw12.absolutefitness.checkoutservice.constants;
 
 public class Constants {
-
     public static final String IN_STOCK ="InStock";
-
     public static final String OUT_OF_STOCK="OutOfStock";
 }
